67352026fee8375a753ccdc342e88b862782085a
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-26 03:13:35 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,11 +11,11 @@ class Anagram
       result
     elsif result.split
     end
-      
+
     array_of_words.each do |word|
       result = word if word.split(//).sort.join == sorted_word
     end
-    result = result.split
+    
     result
   end
 
